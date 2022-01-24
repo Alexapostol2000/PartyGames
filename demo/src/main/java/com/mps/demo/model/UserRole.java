e19611c8077d457bae83c35b46dad57fd640c400
Apostol Alexandru <alexapostol0118@gmail.com>
2022-01-25 00:25:16 +0200

    Specator logic implement

--- demo/src/main/java/com/mps/demo/model/UserRole.java
+++ demo/src/main/java/com/mps/demo/model/UserRole.java
@@ -3,7 +3,8 @@ package com.mps.demo.model;
 public enum UserRole {
 
   ROLE_USER("ROLE_USER"),
-  ROLE_GUEST("NeROLE_GUEST");
+  ROLE_GUEST("NeROLE_GUEST"),
+  ROLE_SPECTATOR("ROLE_SPECTATOR");
 
   public final String value;
 
