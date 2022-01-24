d08ac5087ee5b9abb3e2932390fb009d19ae9ff5
Mircia Camelia <gabrielacamelia1@gmail.com>
2022-01-24 16:05:59 +0200

    fixed string error, design for guess

--- front/partygames/src/Game.js
+++ front/partygames/src/Game.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import Description from './Description';
-
+import img from './emoji.png'
 
 const Game = ({ setToken,  token, adminToken, data }) => {
 
@@ -75,7 +75,7 @@ const Game = ({ setToken,  token, adminToken, data }) => {
 
     return (
         <>
-        <div class = "container"  style={{ width: '20rem', maxHeight: 'rem', margin: '5%',
+        {/* <div class = "container"  style={{ width: '20rem', maxHeight: 'rem', margin: '5%',
         backgroundColor: '#85BAA1', alignContent:'center', borderRadius:'2rem',  borderColor:'#d3bcc0'}}  >
         <div className="card shadow mb-1 mx-auto text-center" />
                   <h5 class="card-title"> <Description roomName={roomName} /> </h5>
@@ -100,8 +100,45 @@ const Game = ({ setToken,  token, adminToken, data }) => {
         <button type="button" className="btn btn-dark" style ={{backgroundColor:'#631D76'}} onClick={() => {
                   
                   handleRound()
-                  console.log("start game")}}>Start Round</button>
-       
+                  console.log("start game")}}>Start Round</button> */}
+
+                  <div class="row">
+                    <div class="col md-4">
+
+                    </div>
+                    <div class="col md-4">
+                    <div class="card" style={{display: 'flex',  justifyContent:'center', alignItems:'center', marginTop:'5rem', backgroundColor: '#85BAA1'}}>
+                            <img className="img" src={img} style ={{maxWidth:'20rem', marginTop:'3rem'}} />
+                      <div class="card-body" > 
+                        <h5 class="card-title"> Guess me! </h5>
+                        <p class="card-text" style ={{ backgroundColor: '#598392', padding:'2 rem',border: '2px solid black',borderRadius:"1rem", padding:"2%"}} > <Description roomName={roomName} /></p>
+                      </div>
+                      <ul class="list-group list-group-flush"  style ={{border: '2px solid black', marginBottom:"3rem", borderRadius:"1rem"}}> 
+                        <li class="list-group-item" style ={{ backgroundColor: '#598392'}}>
+                           <form onSubmit={handleSolution}>
+                          <div className="form-group" >
+                       
+                            <input type="text" className="form-control" placeholder="Enter solution" onChange={e => setSolution(e.target.value)}/>
+                            
+                          </div>
+                          <button type="submit" className="btn btn-dark" style ={{backgroundColor:'#631D76', marginTop:'1rem'}}>Submit</button>
+                          </form>
+                        </li>
+                        <li class="list-group-item" style ={{ backgroundColor: '#598392'}}> 
+                         <button type="button" className="btn btn-dark" style ={{backgroundColor:'#631D76'}} onClick={() => {
+                              handleRound()
+                              console.log("start game")}}>New Round</button></li>
+              
+                       </ul>
+  
+
+</div> 
+                      </div>
+                      <div class="col md-4">
+                      
+                      </div>
+                  </div>
+        
         </>
 
     );}
